88252ab270841769d878c1d1c7c0e196f761fd3d
warramma <wishmorethewitch@gmail.com>
2024-05-13 15:30:27 -0500

    review strings in python

---
+++ introtoPython.py
@@ -0,0 +1,12 @@
+print('Hello world ;)')
+
+character_name = 'Wardiyah'
+age = 19
+pronoun = 'She'
+
+print(character_name + ' is currently ' + str(age) + ' years old.')
+print(pronoun + ' is about to turn ' + str(age + 1) + ' years old.')
+
+hobbies = ['crochet', 'reading', 'learning']
+
+print('Her favorite hobbies are: ' + hobbies[0] + ', ' + hobbies[1] + ', and ' + hobbies[2])
