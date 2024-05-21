c4204298ea8574595dd36109965b46c562a18d2e
warramma <wishmorethewitch@gmail.com>
2024-05-21 14:10:48 -0500

    functions and return statements

---
+++ functions.py
@@ -0,0 +1,13 @@
+#keyword def
+
+def hello (name):
+    print('hello ' + name)
+
+#call the function
+
+hello('Wardiyah')
+
+def return_cube(num):
+    return num * num * num
+
+print(return_cube(3))
