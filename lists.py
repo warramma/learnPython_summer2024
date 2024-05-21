cac94faa8fc1dd271defd8379e7cdd9627ecf84a
warramma <wishmorethewitch@gmail.com>
2024-05-21 13:50:43 -0500

    follow along lists tutorial

---
+++ lists.py
@@ -0,0 +1,39 @@
+#create a list
+#1. give it a name
+#2. set equal to list of values using an array
+
+hobbies = ['crochet', 'reading', 'cleaning','bullet journaling']
+
+#access values with index
+#multitype lists
+
+print(hobbies)
+print(hobbies[1])
+print(hobbies[-1])
+print(hobbies[0:])
+print(hobbies[1:3])
+
+# .extend() - append list to existing list. doesn't have to be same type
+ 
+#.append() - append one item
+
+#.insert(index, value)
+#values get pushed to the right
+
+#.clear() get rid of everything
+
+# .pop() removes last element
+
+#.index(item) = check index of element
+
+#.count()
+
+#.sort()
+
+#.reverse()
+
+#.copy() - copy the list
+
+
+
+
