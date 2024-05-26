049332610c51da29c37fc1c23065e00ba1d1064e
warramma <wishmorethewitch@gmail.com>
2024-05-26 11:43:46 -0500

    convert month abbreviations w dictionary

---
+++ dictionaries.py
@@ -0,0 +1,23 @@
+#5-26-24
+#key-value pairs
+
+#use a dictionary to convert three letter abbreviation for month to full month name
+
+months={
+    "JAN" : "January",
+    "FEB" : "February",
+    "MAR" : "March",
+    "APR" : "April",
+    "MAY" : "May",
+    "JUN" : "June",
+    "JUL" : "July",
+    "AUG" : "August",
+    "SEP" : "September",
+    "OCT" : "October", 
+    "NOV" : "November", 
+    "DEC" : "December"
+}
+
+abbreviation = input('Enter any three letter abbreviation for a month: ').upper()
+
+print("Full month: " + months[abbreviation])
