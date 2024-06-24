694bb34617ad4393ae2d7740e6a032101e3497a7
warramma <wishmorethewitch@gmail.com>
2024-06-23 21:51:20 -0500

    Define Student class

---
+++ classes_and_objects/Student.py
@@ -0,0 +1,13 @@
+#6-23-24
+#create a Student class that keeps track of the Student's current GPA, major(s), year in school, age, first and last name, and whether or not they are on academic probation
+#then, use this class to create Student objects and print students
+
+class Student:
+    
+    def __init__(self, first_name, last_name, gpa, major, year, probation): 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gpa = gpa
+        self.major = major
+        self.year = year
+        self.probation = False
